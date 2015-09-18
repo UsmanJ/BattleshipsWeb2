8ae0cf7218f93cceed4507a102ec75f2127e9f39
Usman Jamil <usman.jamil@live.co.uk>
2015-09-18 16:22:40 +0100

    changed position/orientation global variables to sessions

--- battleships_web.rb
+++ battleships_web.rb
@@ -29,17 +29,17 @@ class BattleshipsWeb < Sinatra::Base
 
   get '/board' do
     if $board1
-      @name = session[:name]
-      $board1.place(Ship.aircraft_carrier, $position1, $orientation1)
-      $board1.place(Ship.battleship, $position2, $orientation2)
-      $board1.place(Ship.destroyer, $position3, $orientation3)
-      $board1.place(Ship.submarine, $position4, $orientation4)
-      $board1.place(Ship.patrol_boat, $position5, $orientation5)
-      # @grid = $board1.print_board
+      $name = session[:name]
+      $board1.place(Ship.aircraft_carrier, session[:position1], params[:orientation1])
+      $board1.place(Ship.battleship, session[:position2], params[:orientation2])
+      $board1.place(Ship.destroyer, session[:position3], params[:orientation3])
+      $board1.place(Ship.submarine, session[:position4], params[:orientation4])
+      $board1.place(Ship.patrol_boat, session[:position5], params[:orientation5])
       erb :board_game_on
     else
       $board1 = Board.new(Cell)
       $player1.board = $board1
+      session[:board1] = $board1
       @name = session[:name]
       @grid = $board1.print_board
       p $game
@@ -48,16 +48,16 @@ class BattleshipsWeb < Sinatra::Base
   end
 
   post '/board' do
-    $position1 = params[:position1].to_sym
-    $orientation1 = params[:orientation1].to_sym
-    $position2 = params[:position2].to_sym
-    $orientation2 = params[:orientation2].to_sym
-    $position3 = params[:position3].to_sym
-    $orientation3 = params[:orientation3].to_sym
-    $position4 = params[:position4].to_sym
-    $orientation4 = params[:orientation4].to_sym
-    $position5 = params[:position5].to_sym
-    $orientation5 = params[:orientation5].to_sym
+    session[:position1] = params[:position1].to_sym
+    session[:orientation1] = params[:orientation1].to_sym
+    session[:position2] = params[:position2].to_sym
+    session[:orientation2] = params[:orientation2].to_sym
+    session[:position3] = params[:position3].to_sym
+    session[:orientation3] = params[:orientation3].to_sym
+    session[:position4] = params[:position4].to_sym
+    session[:orientation4] = params[:orientation4].to_sym
+    session[:position5] = params[:position5].to_sym
+    session[:orientation5] = params[:orientation5].to_sym
     redirect ('/board')
   end
 
